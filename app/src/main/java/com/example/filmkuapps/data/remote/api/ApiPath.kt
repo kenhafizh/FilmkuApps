50a3b3bc67d28f78b475125ee93f925c5c99b7fd
kenhafizh <kenhafiz16@gmail.com>
2026-05-13 11:06:14 +0700

    feat: create api path for api key and base url

---
+++ app/src/main/java/com/example/filmkuapps/data/remote/api/ApiPath.kt
@@ -0,0 +1,7 @@
+package com.example.filmkuapps.data.remote.api
+
+object ApiPath {
+
+    const val API_KEY = "3fd9ccf11cbc0578d66c382288bd7d68"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+}
